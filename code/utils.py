52e7791c5dbeb82fb585157798d0bc10d4c4b395
TomisTony <1158176767@qq.com>
2022-12-13 20:32:42 +0800

    update name

--- code/utils.py
+++ code/utils.py
@@ -27,34 +27,38 @@ class util:
 
     @staticmethod
     def get_pairs_from_combination(data_len_list: list, combination: tuple) -> set[tuple]:
-        init_candidate_list = util.get_init_candidate_list(len(data_len_list))
-        return util.RECUR_get_pairs_from_subcombination(data_len_list, init_candidate_list, combination)
+        init_choosed_element_list = util.get_init_choosed_element_list(len(data_len_list))
+        return util.RECUR_get_pairs_from_subcombination(data_len_list, init_choosed_element_list, combination)
 
     '''
     return the [-1,-1,...] , which length = list_len
     '''
     @staticmethod
-    def get_init_candidate_list(list_len) -> list:
+    def get_init_choosed_element_list(list_len) -> list:
         result = []
         for i in range(list_len):
             result.append(-1)
         return result
 
+    '''
+    Recursive function.
+    Update the choosed_element_list based on the first element in `subcombination`.
+    '''
     @staticmethod
-    def RECUR_get_pairs_from_subcombination(data_len_list: list, candidate_list: list, subcombination: tuple) -> set[tuple]:
+    def RECUR_get_pairs_from_subcombination(data_len_list: list, choosed_element_list: list, subcombination: tuple) -> set[tuple]:
         result = set()
-        candidate_index = subcombination[0] - 1
-        candidate_num = data_len_list[candidate_index]
+        choosed_element_index = subcombination[0] - 1
+        choosed_element_num = data_len_list[choosed_element_index]
 
-        for i in range(candidate_num):
-            candidate_list_impl = candidate_list.copy()
-            candidate_list_impl[candidate_index] = i
+        for i in range(choosed_element_num):
+            choosed_element_list_impl = choosed_element_list.copy()
+            choosed_element_list_impl[choosed_element_index] = i
 
             if (len(subcombination) == 1):
-                candidate_tuple = tuple(candidate_list_impl)
-                result.add(candidate_tuple)
+                choosed_element_tuple = tuple(choosed_element_list_impl)
+                result.add(choosed_element_tuple)
             else:
                 result = result.union(util.RECUR_get_pairs_from_subcombination(
-                    data_len_list, candidate_list_impl, subcombination[1:]))
+                    data_len_list, choosed_element_list_impl, subcombination[1:]))
 
         return sorted(result)
